169732457d542fe145365f7391d7fba4fce37d74
philip-q <kononenko.philip@gmail.com>
2019-12-29 18:59:33 +0200

    initial electron app

---
+++ start.js
@@ -0,0 +1,54 @@
+const electron = require('electron');
+const app = electron.app;
+const dialog = electron.dialog;
+const { BrowserWindow } = electron;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+require('electron-reload');
+
+const fs = require('fs');
+// console.log("FS:::: ", fs);
+
+console.log("!!!!");
+
+function handle_custom_message() {
+    console.log("Handling custom message")
+}
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    let url = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
+    console.log("url is : ", url);
+    mainWindow.loadURL(url);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    });
+
+    mainWindow.webContents.openDevTools()
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
+
